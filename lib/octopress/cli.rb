694283f3aea59e771485ab6710d4b6335236ba02
Raving Genius <rg+code@ravinggenius.com>
2011-06-27 23:44:45 -0400

    Update comment concerning content types

--- lib/octopress/cli.rb
+++ lib/octopress/cli.rb
@@ -40,9 +40,10 @@ Help message goes here
 
     # content types are a bit messy to deal with. using classes to represent each content type
     # allows for future custom behavior to be added per type relatively easily. however this
-    # introduces more issues when adding custom types
+    # introduces more issues when adding custom types (how to register new types?)
+    #
     # alternatively a content type could be based simply on the files (including directories)
-    # within user-configurable directories. this would allow simple discovery of new types, but
+    # within user-configurable directory(ies). this would allow simple discovery of new types, but
     # would require the file name patter for each type to be stored in the config
     def create(content_type, title)
       content_types = Octopress::ContentType.types
