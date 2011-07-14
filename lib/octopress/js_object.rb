67c62b4f60f34def1ad5eac297dcf251c40cb543
Raving Genius <rg+code@ravinggenius.com>
2011-07-14 19:17:10 -0400

    Add a class to mimic JavaScript's Object

---
+++ lib/octopress/js_object.rb
@@ -0,0 +1,47 @@
+require 'ostruct'
+
+require 'octopress'
+
+module Octopress
+  class JSObject < OpenStruct
+    def to_hash
+      handler = lambda do |v|
+        if v.is_an? Array
+          v.map { |vv| handler.call vv }
+        elsif v.is_a? JSObject
+          v.to_hash
+        else
+          v
+        end
+      end
+
+      converted = {}
+
+      @table.each do |key, value|
+        converted[key] = handler.call value
+      end
+
+      converted
+    end
+
+    def self.new(hash = {})
+      handler = lambda do |v|
+        if v.is_an? Array
+          v.map { |vv| handler.call vv }
+        elsif v.is_a? Hash
+          new v
+        else
+          v
+        end
+      end
+
+      converted = {}
+
+      hash.each do |key, value|
+        converted[key] = handler.call value
+      end
+
+      super converted
+    end
+  end
+end
