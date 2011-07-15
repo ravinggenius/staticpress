a35bf0070ec587452efa6779ae8ab1268b8a6712
Raving Genius <rg+code@ravinggenius.com>
2011-07-14 20:32:28 -0400

    Modify configuration class to inherit from JSObject

--- lib/octopress/configuration.rb
+++ lib/octopress/configuration.rb
@@ -1,14 +1,10 @@
-require 'ostruct'
 require 'yaml'
 
 require 'octopress'
+require 'octopress/js_object'
 
 module Octopress
-  class Configuration < OpenStruct
-    def to_hash
-      @table.clone
-    end
-
+  class Configuration < JSObject
     def save
       (Octopress.blog_path + 'config.yml').open('w') { |f| YAML.dump(to_hash, f) }
     end
