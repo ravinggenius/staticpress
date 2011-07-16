43b1b3c3b46858fd24591f35f9192fac688696ac
Raving Genius <rg+code@ravinggenius.com>
2011-07-16 16:40:09 -0400

    Add one more assertion to HelpersTest#test_extensionless_basename

--- tests/lib/octopress/helpers_test.rb
+++ tests/lib/octopress/helpers_test.rb
@@ -8,6 +8,7 @@ class HelpersTest < MiniTest::Unit::TestCase
   def test_extensionless_basename
     assert_equal '.htaccess', extensionless_basename(Pathname.new('.htaccess'))
     assert_equal 'tyrannasaurus_rex', extensionless_basename(Pathname.new('tyrannasaurus_rex.rb'))
+    assert_equal 'extensionless', extensionless_basename(Pathname.new('extensionless'))
   end
 
   def test_route_options
