f086842da3361473627189c9753aa4ced4df9a37
Raving Genius <rg+code@ravinggenius.com>
2011-07-16 17:02:39 -0400

    Auto discover tests when with tests/runner.rb

--- tests/runner.rb
+++ tests/runner.rb
@@ -1,13 +1,13 @@
-require_relative 'lib/octopress_test'
-require_relative 'lib/octopress/cli_test'
-require_relative 'lib/octopress/configuration_test'
-require_relative 'lib/octopress/helpers_test'
-require_relative 'lib/octopress/js_object_test'
-require_relative 'lib/octopress/metadata_test'
-require_relative 'lib/octopress/plugin_test'
-require_relative 'lib/octopress/server_test'
-require_relative 'lib/octopress/site_test'
-require_relative 'lib/octopress/theme_test'
-require_relative 'lib/octopress/content/base_test'
-require_relative 'lib/octopress/content/page_test'
-require_relative 'lib/octopress/content/post_test'
+require 'pathname'
+
+def run_recursively(dir)
+  dir.children.each do |dir_or_test|
+    if dir_or_test.directory?
+      run_recursively dir_or_test
+    elsif dir_or_test.to_s.end_with? '_test.rb'
+      require_relative dir_or_test
+    end
+  end
+end
+
+run_recursively Pathname.new('tests/lib').expand_path
