5400dfa7dd3f1830a99574a585c75ad6af5563fb
Raving Genius <rg+code@ravinggenius.com>
2011-07-16 16:24:03 -0400

    Refactor JSObject to keep it DRY

--- lib/octopress/js_object.rb
+++ lib/octopress/js_object.rb
@@ -9,43 +9,35 @@ module Octopress
     end
 
     def to_hash
-      handler = lambda do |v|
-        if v.is_a? Array
-          v.map { |vv| handler.call vv }
-        elsif v.is_a? JSObject
-          v.to_hash
-        else
-          v
-        end
-      end
-
       converted = {}
 
       @table.each do |key, value|
-        converted[key] = handler.call value
+        converted[key] = self.class.converter(value, JSObject) { |v| v.to_hash }
       end
 
       converted
     end
 
     def self.new(hash = {})
-      handler = lambda do |v|
-        if v.is_a? Array
-          v.map { |vv| handler.call vv }
-        elsif v.is_a? Hash
-          new v
-        else
-          v
-        end
-      end
-
       converted = {}
 
       hash.each do |key, value|
-        converted[key] = handler.call value
+        converted[key] = converter(value, Hash) { |v| new v }
       end
 
       super converted
+    end
+
+    protected
+
+    def self.converter(value, from_class, &block)
+      if value.is_a? Array
+        value.map { |vv| converter.call vv, from_class, block }
+      elsif value.is_a? from_class
+        block.call value
+      else
+        value
+      end
     end
   end
 end
