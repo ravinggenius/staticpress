047f981aae2b5437f9bb5790d24c71821ae3b19e
Raving Genius <rg+code@ravinggenius.com>
2011-07-18 19:22:09 -0400

    Fill in theme tests

--- tests/lib/octopress/theme_test.rb
+++ tests/lib/octopress/theme_test.rb
@@ -12,8 +12,10 @@ class ThemeTest < MiniTest::Unit::TestCase
   end
 
   def test_keyed_layouts
+    assert_equal({ 'default' => (@theme.root + '_layouts' + 'default.haml') }, @theme.keyed_layouts)
   end
 
   def test_layouts
+    assert_equal 1, @theme.layouts.count
   end
 end
