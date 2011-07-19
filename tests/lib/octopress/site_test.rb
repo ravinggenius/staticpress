158172a74b82dc61a48c32cbc78a1461f39a49f2
Raving Genius <rg+code@ravinggenius.com>
2011-07-18 23:15:47 -0400

    Add a little test for routes

--- tests/lib/octopress/site_test.rb
+++ tests/lib/octopress/site_test.rb
@@ -1,7 +1,17 @@
 require_relative '../../test_helper'
 
+require 'octopress/site'
+
 class SiteTest < MiniTest::Unit::TestCase
+  READONLY = SAMPLE_SITES + 'readonly'
+
   def setup
+    Octopress.blog_path = READONLY
     @site = Octopress::Site.new
+  end
+
+  def test_find_page_by_route
+    about = Octopress::Content::Page.new(@site.directory + 'about.markdown')
+    assert_equal about.route, @site.find_page_by_route('/about').route
   end
 end
