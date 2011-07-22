9c5907fbcb915805d6c39c772a01b801c7a51300
Raving Genius <rg+code@ravinggenius.com>
2011-07-21 21:44:45 -0400

    Order arguments correctly for assert_equal (expected, actual)

--- tests/lib/octopress/js_object_test.rb
+++ tests/lib/octopress/js_object_test.rb
@@ -9,23 +9,23 @@ class JSObjectTest < TestHelper
 
   def test__squares
     assert_nil @js_object[:some_random]
-    assert_equal @js_object[:key], :value
-    assert_equal @js_object['key'], :value
+    assert_equal :value, @js_object[:key]
+    assert_equal :value,  @js_object['key']
   end
 
   def test_regular_access
-    assert_equal @js_object.key, :value
-    assert_equal @js_object.nested.a, :b
+    assert_equal :value, @js_object.key
+    assert_equal :b, @js_object.nested.a
     assert_nil @js_object.nested.other
   end
 
   def test_assignment
     assert_nil @js_object.hoopla
     @js_object.hoopla = :hullabaloo
-    assert_equal @js_object.hoopla, :hullabaloo
+    assert_equal :hullabaloo, @js_object.hoopla
   end
 
   def test_to_hash
-    assert_equal @js_object.to_hash, { :key => :value, :nested => { :a => :b } }
+    assert_equal({ :key => :value, :nested => { :a => :b } }, @js_object.to_hash)
   end
 end
