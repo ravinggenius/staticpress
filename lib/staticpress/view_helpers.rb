777983745e930e051134e95a2b58a14998457d9f
Raving Genius <rg+code@ravinggenius.com>
2011-08-12 00:35:54 -0400

    Finally implement Staticpress::ViewHelpers#partial

--- lib/staticpress/view_helpers.rb
+++ lib/staticpress/view_helpers.rb
@@ -7,5 +7,13 @@ module Staticpress
     def initialize(theme)
       @theme = theme
     end
+
+    def partial(name, locals = {})
+      template_name = theme.include_for name
+      if template_name.file?
+        template = Tilt[template_name].new { template_name.read }
+        template.render self.class.new(theme), locals
+      end
+    end
   end
 end
--- tests/lib/staticpress/view_helpers_test.rb
+++ tests/lib/staticpress/view_helpers_test.rb
@@ -3,4 +3,27 @@ require_relative '../../test_helper'
 require 'staticpress/view_helpers'
 
 class ViewHelpersTest < TestHelper
+  def setup
+    Staticpress.blog_path = TEST_BLOG
+    @post_route = Staticpress::Route.from_url_path '/2011/07/20/hello'
+    @post = Staticpress::Content::Post.new @post_route, :markdown
+    @view_helpers = Staticpress::ViewHelpers.new @post.theme
+  end
+
+  def test_partial_with_one_post
+    expected = <<-HTML
+<section>
+  <article>#{@post.render_partial.strip}</article>
+</section>
+    HTML
+    assert_equal expected, @view_helpers.partial(:list_posts, :posts => [ @post ])
+  end
+
+  def test_partial_with_no_posts
+    expected = <<-HTML
+<section>
+</section>
+    HTML
+    assert_equal expected, @view_helpers.partial(:list_posts, :posts => [ ])
+  end
 end
