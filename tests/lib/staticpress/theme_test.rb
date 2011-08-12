bf911d178559f3b073911ff3ef38d75ae7abd979
Raving Genius <rg+code@ravinggenius.com>
2011-08-11 23:19:24 -0400

    Expand Staticpress::Theme tests

--- tests/lib/staticpress/theme_test.rb
+++ tests/lib/staticpress/theme_test.rb
@@ -7,14 +7,20 @@ class ThemeTest < TestHelper
     @theme = Staticpress::Theme.new :classic
   end
 
+  def test_default_layout
+    assert_equal (@theme.root + '_layouts' + 'default.haml'), @theme.default_layout
+  end
+
   def test_keyed_layouts
     assert_equal((@theme.root + '_layouts' + 'default.haml'), @theme.keyed_layouts['default'])
     assert_equal((@theme.root + '_layouts' + 'post_index.haml'), @theme.keyed_layouts['post_index'])
+    assert_nil @theme.keyed_layouts['fake']
   end
 
   def test_layout_for
     assert_equal (@theme.root + '_layouts' + 'default.haml'), @theme.layout_for(:default)
     assert_equal (@theme.root + '_layouts' + 'post_index.haml'), @theme.layout_for(:post_index)
+    assert_equal (@theme.root + '_layouts' + 'default.haml'), @theme.layout_for(:fake)
   end
 
   def test_layouts
