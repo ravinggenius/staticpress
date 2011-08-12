c74396b90a044d07e7684d09c22b722e11d594b3
Raving Genius <rg+code@ravinggenius.com>
2011-08-11 23:17:19 -0400

    Test methods in order defined

--- tests/lib/staticpress/theme_test.rb
+++ tests/lib/staticpress/theme_test.rb
@@ -7,14 +7,14 @@ class ThemeTest < TestHelper
     @theme = Staticpress::Theme.new :classic
   end
 
-  def test_layout_for
-    assert_equal (@theme.root + '_layouts' + 'default.haml'), @theme.layout_for(:default)
-    assert_equal (@theme.root + '_layouts' + 'post_index.haml'), @theme.layout_for(:post_index)
-  end
-
   def test_keyed_layouts
     assert_equal((@theme.root + '_layouts' + 'default.haml'), @theme.keyed_layouts['default'])
     assert_equal((@theme.root + '_layouts' + 'post_index.haml'), @theme.keyed_layouts['post_index'])
+  end
+
+  def test_layout_for
+    assert_equal (@theme.root + '_layouts' + 'default.haml'), @theme.layout_for(:default)
+    assert_equal (@theme.root + '_layouts' + 'post_index.haml'), @theme.layout_for(:post_index)
   end
 
   def test_layouts
