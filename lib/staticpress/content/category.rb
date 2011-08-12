2442700da4a7ca06d4117fb5cf61af37acbee22c
Raving Genius <rg+code@ravinggenius.com>
2011-08-11 20:47:39 -0400

    Lazy commit for category content

--- lib/staticpress/content/category.rb
+++ lib/staticpress/content/category.rb
@@ -8,10 +8,24 @@ module Staticpress::Content
       { :text => template_path.read }
     end
 
-    def self.all
+    def sub_content
       Staticpress::Content::Post.all.map do |post|
-        post if post.meta.categories
+        post if post.meta.categories && post.meta.categories.include?(route.params[:name])
       end.compact
+    end
+
+    def self.all
+      categories.map { |category| find_by_category category }
+    end
+
+    def self.categories
+      Staticpress::Content::Post.all.map do |post|
+        post.meta.categories
+      end.compact.flatten.uniq
+    end
+
+    def self.find_by_category(category)
+      new Staticpress::Route.new(:content_type => self, :name => category), template_path.extname.sub('.', '').to_sym
     end
   end
 end
--- tests/lib/staticpress/content/category_test.rb
+++ tests/lib/staticpress/content/category_test.rb
@@ -16,8 +16,16 @@ class ContentCategoryTest < ContentBaseTest
     refute_operator @category_page, :==, nil
   end
 
+  def test_categories
+    assert_equal [ 'programming' ], Staticpress::Content::Category.categories
+  end
+
   def test_exist?
     assert @category_page.exist?, '@category_page does not exist'
+  end
+
+  def test_find_by_category
+    assert_equal @category_page, Staticpress::Content::Category.find_by_category('programming')
   end
 
   def test_find_by_route
@@ -28,11 +36,20 @@ class ContentCategoryTest < ContentBaseTest
     assert_equal '#<Staticpress::Content::Category url_path=/category/programming>', @category_page.inspect
   end
 
+  def test_sub_content
+    assert_equal 3, @category_page.sub_content.count
+  end
+
   def test_raw
     assert_equal '= partial :list_posts, :posts => page.sub_content', @category_page.raw
   end
 
   def test_route
     assert_equal '/category/programming', @category_page.route.url_path
+  end
+
+  def test_all
+    assert_equal 1, Staticpress::Content::Category.all.count
+    assert Staticpress::Content::Category.all.include?(@category_page)
   end
 end
