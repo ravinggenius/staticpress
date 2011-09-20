934f7e6cc0e53ef1615876d87d670688a42b950a
Raving Genius <rg+code@ravinggenius.com>
2011-09-20 16:55:46 -0400

    Refactor Staticpress::Configuration to consider default values separate from custom values

--- lib/staticpress/configuration.rb
+++ lib/staticpress/configuration.rb
@@ -6,11 +6,18 @@ require 'staticpress/js_object'
 module Staticpress
   class Configuration < JSObject
     def save
-      (Staticpress.blog_path + 'config.yml').open('w') { |f| YAML.dump(to_hash, f) }
+      (Staticpress.blog_path + 'config.yml').open('w') do |f|
+        custom_values = self - self.class.default
+        YAML.dump(custom_values.to_hash, f)
+      end
+    end
+
+    def self.default
+      @default ||= new(YAML.load_file(Staticpress.root + 'skeleton' + 'config.yml'))
     end
 
     def self.instance
-      @config ||= new(YAML.load_file(Staticpress.blog_path + 'config.yml'))
+      @config ||= new(default.to_hash.merge(YAML.load_file(Staticpress.blog_path + 'config.yml')))
     end
   end
 end
