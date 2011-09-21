ravinggenius/staticpress

commit 26d98af4b6c6142803c8add423a31b5c0143e736

Overhaul Staticpress::Content::Category

lib/staticpress/content/category.rb
@@ -5,25 +5,27 @@ require 'staticpress/route'
 module Staticpress::Content
   class Category < CollectionContent
     def sub_content
-      Staticpress::Content::Post.all.map do |post|
-        post if post.meta.categories && post.meta.categories.include?(route.params[:name])
-      end.compact
+      paginate(self.class.content_by_category[route.params[:name]])[(Integer route.params[:number]) - 1]
     end
 
     def self.all
-      categories.map { |category| find_by_category category }
+      categories.map do |category|
+        find_by_route Staticpress::Route.new(:content_type => self, :name => category, :number => 1)
+      end
     end
 
     def self.categories
-      Staticpress::Content::Post.all.map do |post|
-        post.meta.categories
-      end.compact.flatten.uniq
+      content_by_category.keys
     end
 
-    # FIXME needs to return an array
-    def self.find_by_category(category)
-      # TODO find how many posts and return appropriate number of category pages
-      find_by_route Staticpress::Route.new(:content_type => self, :name => category, :number => nil)
+    def self.content_by_category
+      reply = {}
+      Staticpress::Content::Post.all.each do |post|
+        (post.meta.categories || []).each do |category|
+          (reply[category] ||= []) << post
+        end
+      end
+      reply
     end
   end
 end

tests/lib/staticpress/content/category_test.rb
@@ -26,10 +26,6 @@ class ContentCategoryTest < ContentBaseTest
     assert @category_page.exist?, '@category_page does not exist'
   end
 
-  def test_find_by_category
-    assert_equal @category_page, Staticpress::Content::Category.find_by_category('programming')
-  end
-
   def test_find_by_route
     assert_equal @category_page, Staticpress::Content::Category.find_by_route(@category_page_route)
   end

commit b427175499e2f141918e0a82910316eee1d7c92b

Fix issue introduced in commit 26d98af and remove inaccurate error message from commit 3e3ee9c

lib/staticpress/content/category.rb
@@ -10,7 +10,7 @@ module Staticpress::Content
 
     def self.all
       categories.map do |category|
-        find_by_route Staticpress::Route.new(:content_type => self, :name => category, :number => 1)
+        find_by_route Staticpress::Route.new(:content_type => self, :name => category, :number => '1')
       end
     end
 

tests/lib/staticpress/content/category_test.rb
@@ -49,6 +49,6 @@ class ContentCategoryTest < ContentBaseTest
 
   def test_all
     assert_equal 1, Staticpress::Content::Category.all.count
-    assert Staticpress::Content::Category.all.include?(@category_page), "#{Staticpress::Content::Category.all.inspect} does not include #{@category_page.inspect}"
+    assert Staticpress::Content::Category.all.include?(@category_page)
   end
 end