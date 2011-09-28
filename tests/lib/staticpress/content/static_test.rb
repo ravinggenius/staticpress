4f94f6323c0cb412688da6ba3c81f76934a885e0
Raving Genius <rg+code@ravinggenius.com>
2011-09-28 16:17:00 -0400

    Add new content type to handle static files (images etc)

---
+++ tests/lib/staticpress/content/static_test.rb
@@ -0,0 +1,57 @@
+require_relative 'base_test'
+
+require 'staticpress/content/static'
+require 'staticpress/helpers'
+require 'staticpress/route'
+
+class ContentStaticTest < ContentBaseTest
+  include Staticpress::Helpers
+
+  def setup
+    super
+
+    @static_dir = Staticpress.blog_path + config.source
+
+    @static_route = Staticpress::Route.from_url_path '/ruby.png'
+    @static = Staticpress::Content::Static.new @static_route, @static_dir + 'ruby.png'
+
+    puts
+    puts @static_route.inspect
+  end
+
+  def test__equalsequals
+    assert_operator @static, :==, Staticpress::Content::Static.new(@static_route, @static_dir + 'ruby.png')
+    refute_operator @static, :==, nil
+  end
+
+  def test_exist?
+    assert @static.exist?, '@static does not exist'
+  end
+
+  def test_find_by_path
+    @static_dir = Staticpress.blog_path + config.source
+    assert_equal @static, Staticpress::Content::Static.find_by_path(@static_dir + 'ruby.png')
+    assert_nil Staticpress::Content::Static.find_by_path(@static_dir + 'i' + 'dont' + 'exist.jpg')
+  end
+
+  def test_find_by_route
+    assert_equal @static, Staticpress::Content::Static.find_by_route(@static_route)
+    assert_nil Staticpress::Content::Static.find_by_route(nil)
+  end
+
+  def test_inspect
+    assert_equal '#<Staticpress::Content::Static url_path=/ruby.png>', @static.inspect
+  end
+
+  def test_raw
+    assert_equal 'in static', @static.raw
+  end
+
+  def test_render_partial
+    assert_equal "<p>in static</p>\n", @static.render_partial
+  end
+
+  def test_route
+    assert_equal '/ruby.png', @static.route.url_path
+  end
+end
