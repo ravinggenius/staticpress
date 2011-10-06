fb756962c40c086cc295ede0497304bcf6c02c0a
Raving Genius <rg+code@ravinggenius.com>
2011-10-06 00:03:41 -0400

    Add plain text file for simpler Staticpress::Content::Static testing

--- tests/lib/staticpress/content/static_test.rb
+++ tests/lib/staticpress/content/static_test.rb
@@ -1,6 +1,7 @@
 require_relative 'base_test'
 
 require 'staticpress/content/static'
+require 'staticpress/error'
 require 'staticpress/helpers'
 require 'staticpress/route'
 
@@ -12,40 +13,47 @@ class ContentStaticTest < ContentBaseTest
 
     @static_dir = Staticpress.blog_path + config.source
 
-    @static_route = Staticpress::Route.from_url_path '/ruby.png'
-    @static = Staticpress::Content::Static.new @static_route, @static_dir + 'ruby.png'
+    @static_bin_route = Staticpress::Route.from_url_path '/ruby.png'
+    @static_bin = Staticpress::Content::Static.new @static_bin_route, @static_dir + 'ruby.png'
+
+    @static_txt_route = Staticpress::Route.from_url_path '/plain.txt'
+    @static_txt = Staticpress::Content::Static.new @static_txt_route, @static_dir + 'plain.txt'
   end
 
   def test__equalsequals
-    assert_operator @static, :==, Staticpress::Content::Static.new(@static_route, @static_dir + 'ruby.png')
-    refute_operator @static, :==, nil
+    assert_operator @static_bin, :==, Staticpress::Content::Static.new(@static_bin_route, @static_dir + 'ruby.png')
+    refute_operator @static_bin, :==, nil
+    refute_operator @static_bin, :==, @static_txt
   end
 
   def test_exist?
-    assert @static.exist?, '@static does not exist'
+    assert @static_bin.exist?, '@static_bin does not exist'
+    assert @static_txt.exist?, '@static_txt does not exist'
   end
 
   def test_find_by_path
-    assert_equal @static, Staticpress::Content::Static.find_by_path(@static_dir + 'ruby.png')
+    assert_equal @static_bin, Staticpress::Content::Static.find_by_path(@static_dir + 'ruby.png')
     assert_nil Staticpress::Content::Static.find_by_path(@static_dir + 'i' + 'dont' + 'exist.jpg')
   end
 
   def test_find_by_route
-    assert_equal @static, Staticpress::Content::Static.find_by_route(@static_route)
+    assert_equal @static_bin, Staticpress::Content::Static.find_by_route(@static_bin_route)
     assert_nil Staticpress::Content::Static.find_by_route(nil)
   end
 
   def test_inspect
-    assert_equal '#<Staticpress::Content::Static url_path=/ruby.png>', @static.inspect
+    assert_equal '#<Staticpress::Content::Static url_path=/ruby.png>', @static_bin.inspect
   end
 
   def test_raw
+    assert_equal 'this file intentionally left blank', @static_txt.raw
   end
 
   def test_render_partial
   end
 
   def test_route
-    assert_equal '/ruby.png', @static.route.url_path
+    assert_equal '/ruby.png', @static_bin.route.url_path
+    assert_equal '/plain.txt', @static_txt.route.url_path
   end
 end
