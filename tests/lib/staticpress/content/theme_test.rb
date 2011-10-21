21cdfa25c7a41d238e2f8c832a538c081c23823f
Raving Genius <rg+code@ravinggenius.com>
2011-10-20 23:48:47 -0400

    Rename instance variables in ContentThemeTest

--- tests/lib/staticpress/content/theme_test.rb
+++ tests/lib/staticpress/content/theme_test.rb
@@ -14,60 +14,60 @@ class ContentThemeTest < ContentBaseTest
 
     @theme_dir = Staticpress::Theme.theme.root
 
-    @asset_route = Staticpress::Route.from_url_path '/assets/classic/styles/all'
-    @asset = Staticpress::Content::Theme.new @asset_route, @theme_dir + 'assets' + 'styles' + 'all.sass'
+    @asset_style_route = Staticpress::Route.from_url_path '/assets/classic/styles/all'
+    @asset_style = Staticpress::Content::Theme.new @asset_style_route, @theme_dir + 'assets' + 'styles' + 'all.sass'
   end
 
   def test__equalsequals
-    assert_operator @asset, :==, Staticpress::Content::Theme.new(@asset_route, @theme_dir + 'assets' + 'styles' + 'all.sass')
-    refute_operator @asset, :==, nil
+    assert_operator @asset_style, :==, Staticpress::Content::Theme.new(@asset_style_route, @theme_dir + 'assets' + 'styles' + 'all.sass')
+    refute_operator @asset_style, :==, nil
   end
 
   def test_all
-    assert_equal 1, Staticpress::Content::Theme.all.count
+    assert_equal 2, Staticpress::Content::Theme.all.count
   end
 
   def test_exist?
-    assert @asset.exist?, '@asset does not exist'
+    assert @asset_style.exist?, '@asset_style does not exist'
   end
 
   def test_find_by_path
-    assert_equal @asset, Staticpress::Content::Theme.find_by_path(@theme_dir + 'assets' + 'styles' + 'all.sass')
+    assert_equal @asset_style, Staticpress::Content::Theme.find_by_path(@theme_dir + 'assets' + 'styles' + 'all.sass')
     assert_nil Staticpress::Content::Theme.find_by_path(@theme_dir + 'i' + 'dont' + 'exist.markdown')
   end
 
   def test_find_by_route
-    assert_equal @asset, Staticpress::Content::Theme.find_by_route(@asset_route)
+    assert_equal @asset_style, Staticpress::Content::Theme.find_by_route(@asset_style_route)
     assert_nil Staticpress::Content::Theme.find_by_route(nil)
   end
 
   def test_inspect
-    assert_equal '#<Staticpress::Content::Theme url_path=/assets/classic/styles/all>', @asset.inspect
+    assert_equal '#<Staticpress::Content::Theme url_path=/assets/classic/styles/all>', @asset_style.inspect
   end
 
   def test_output_path
-    assert_equal (Staticpress.blog_path + 'public' + 'assets' + 'classic' + 'styles' + 'all'), @asset.output_path
+    assert_equal (Staticpress.blog_path + 'public' + 'assets' + 'classic' + 'styles' + 'all'), @asset_style.output_path
   end
 
   def test_raw
     expected = <<-SASS
     SASS
-    assert_equal expected, @asset.raw
+    assert_equal expected, @asset_style.raw
   end
 
   def test_render
     expected = <<-SASS
     SASS
-    assert_equal expected, @asset.render
+    assert_equal expected, @asset_style.render
   end
 
   def test_render_partial
     expected = <<-SASS
     SASS
-    assert_equal expected, @asset.render_partial
+    assert_equal expected, @asset_style.render_partial
   end
 
   def test_route
-    assert_equal '/assets/classic/styles/all', @asset.route.url_path
+    assert_equal '/assets/classic/styles/all', @asset_style.route.url_path
   end
 end
