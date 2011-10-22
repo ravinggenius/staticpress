3e2d5f54375f12de4fe5051b8d21d96e3bbf8aac
Raving Genius <rg+code@ravinggenius.com>
2011-10-22 11:10:00 -0400

    Add note for future reference

--- lib/staticpress/content/collection_content.rb
+++ lib/staticpress/content/collection_content.rb
@@ -2,6 +2,7 @@ require 'staticpress'
 require 'staticpress/content/base'
 
 module Staticpress::Content
+  # TODO convert to module
   class CollectionContent < Base
     def self.find_by_route(route)
       new(route, template_path)
