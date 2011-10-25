545a1caeb53cff96ae7f42b8facdb4ca86bb8e45
Raving Genius <rg+code@ravinggenius.com>
2011-10-24 23:55:00 -0400

    Version bump

--- lib/staticpress/version.rb
+++ lib/staticpress/version.rb
@@ -2,8 +2,8 @@ module Staticpress
   # TODO figure out how to implement Gem::Version properly
   class Version
     MAJOR = 0
-    MINOR = 1
-    PATCH = 3
+    MINOR = 2
+    PATCH = 0
 
     def self.to_s
       [ MAJOR, MINOR, PATCH ].join '.'
