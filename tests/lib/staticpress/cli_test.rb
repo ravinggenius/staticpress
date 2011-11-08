d41c2c0f99c367431e52811b0f442da191c091f5
Raving Genius <rg+code@ravinggenius.com>
2011-11-07 19:55:53 -0500

    Remove empty test methods

--- tests/lib/staticpress/cli_test.rb
+++ tests/lib/staticpress/cli_test.rb
@@ -9,8 +9,6 @@ require 'staticpress/helpers'
 class CLITest < TestHelper
   include Staticpress::Helpers
 
-  TEMP_BLOG = SAMPLE_SITES + 'temp_blog'
-
   def setup
     Staticpress.blog_path = TEMP_BLOG
     @cli = Staticpress::CLI.new
@@ -19,9 +17,6 @@ class CLITest < TestHelper
   def teardown
     FileUtils.rm_rf TEMP_BLOG if TEMP_BLOG.directory?
     super
-  end
-
-  def test_help
   end
 
   # FIXME breaks ContentThemeTest#test_template_engine_options
@@ -36,32 +31,5 @@ class CLITest < TestHelper
   def _test_new_with_custom_title
     @cli.new TEMP_BLOG, 'This is my blog'
     assert_equal 'This is my blog', config.title
-  end
-
-  def test_create
-  end
-
-  def test_create_page
-  end
-
-  def test_fork_plugin
-  end
-
-  def test_fork_theme
-  end
-
-  def test_build
-  end
-
-  def test_serve
-  end
-
-  def test_push
-  end
-
-  def test_deploy
-  end
-
-  def test_version
   end
 end
