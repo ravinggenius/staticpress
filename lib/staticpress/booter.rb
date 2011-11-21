b90bfcd1541165c8a4e95492285033e594a80e24
Raving Genius <rg+code@ravinggenius.com>
2011-11-20 22:01:04 -0500

    Adjust lib/staticpress/booter.rb

--- lib/staticpress/booter.rb
+++ lib/staticpress/booter.rb
@@ -1,6 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('./Gemfile')
 
-if File.exists?(ENV['BUNDLE_GEMFILE']) && !defined? Bundler
+if File.exists?(ENV['BUNDLE_GEMFILE'])
   require 'bundler'
 
   Bundler.require
