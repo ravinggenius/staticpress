b18f2628b6aa860c06775e7c933dcbcb9ce7584d
Raving Genius <rg+code@ravinggenius.com>
2012-11-09 20:36:34 -0500

    Require support files and fix config typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'sass'
 
 require_relative '../lib/staticpress'
 
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |path| require_relative path }
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -23,16 +25,17 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 
+  config.include CustomMatchers
   config.include UnitSupport
   config.include IntegrationSupport
 
   TEST_BLOG = (Staticpress.root + '..' + 'tests' + 'test_blog').expand_path
 
-  before :each do
+  config.before :each do
     Staticpress.blog_path = TEST_BLOG
   end
 
-  after :each do
+  config.after :each do
     Staticpress.blog_path = '.'
     test_blog_public = TEST_BLOG + 'public'
     FileUtils.rm_rf test_blog_public if test_blog_public.directory?
