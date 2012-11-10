6f82f541dd31b05ad194f864ff2868299dd9e341
Raving Genius <rg+code@ravinggenius.com>
2012-11-10 00:02:49 -0500

    Remove empty test files

--- spec/unit/staticpress/configuration_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-describe Staticpress::Configuration do
-end
--- spec/unit/staticpress/pusher_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Staticpress::Pusher do
-  def test_custom
-  end
-
-  def test_push
-  end
-end
