5f296d8c463a351659f6aebfa119997024a2232f
Raving Genius <rg+code@ravinggenius.com>
2012-12-19 23:00:58 -0500

    Fix recently broken test

--- spec/integration/editing_blog_spec.rb
+++ spec/integration/editing_blog_spec.rb
@@ -23,8 +23,14 @@ describe 'Editing blog' do
     confirm_plugin_editable 'pullquote'
   end
 
-  it 'Copying the default theme' do
-    fork_theme
-    confirm_theme_is_local 'basic'
+  context 'without a custom theme' do
+    before :each do
+      run_simple 'rm -fR themes'
+    end
+
+    it 'Copying the default theme' do
+      fork_theme
+      confirm_theme_is_local 'basic'
+    end
   end
 end
